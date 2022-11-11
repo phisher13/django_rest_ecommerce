31f1212b5c52d2e677f81cddfdadc0b47044e4bf
phisher13 <channektoshka@gmail.com>
2022-11-11 19:28:43 +0200

    [FIX]: fix endpoint

--- django_rest_ecommerce/urls.py
+++ django_rest_ecommerce/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
-    path('', include('ecommerce.urls')),
+    path('api/v1/', include('ecommerce.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
