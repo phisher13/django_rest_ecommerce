2411c638afe7aeaef5dc37c9b8e0ab498115f9d7
phisher13 <channektoshka@gmail.com>
2022-11-11 19:29:59 +0200

    [ADD]: create views

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from .models import Product
+from .serializer import ProductSerializer
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    lookup_field = 'slug'
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
