1fd544cf3ae9c2af7eddc2c7ee1de9db18442a86
phisher13 <channektoshka@gmail.com>
2022-11-12 19:13:06 +0200

    [ADD]: add product to favourite list

--- ecommerce/services.py
+++ ecommerce/services.py
@@ -1,5 +1,5 @@
-from .models import Product
-from .serializer import ProductSerializer
+from .models import Product, Favourite
+from .serializer import ProductSerializer, FavouritesCreateSerializer
 
 
 def get_serializable_queryset(category_slug: str, product_slug: str = None) -> dict:
@@ -10,4 +10,18 @@ def get_serializable_queryset(category_slug: str, product_slug: str = None) -> d
         products = Product.objects.all().filter(category__slug=category_slug)
         serializer = ProductSerializer(instance=products, many=True)
 
+    return serializer.data
+
+
+def add_products_to_favourite(request):
+    user = request.user
+    favourite = Favourite.objects.filter(user=user).first()
+    if favourite:
+        favourite.products.add(request.data['product'])
+    else:
+        favourite = Favourite.objects.create(
+            user=user
+        )
+        favourite.products.add(request.data['product'])
+    serializer = FavouritesCreateSerializer(instance=favourite)
     return serializer.data
