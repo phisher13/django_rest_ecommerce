5758ab24b46984e1e997b895831d7188a0f6e5f5
phisher13 <channektoshka@gmail.com>
2022-11-12 16:05:08 +0200

    [UPD]: category & product view's methods

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,17 +1,57 @@
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+import http
+
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+    CreateAPIView
+)
 from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView, Response
 
-from .models import Product
-from .serializer import ProductSerializer
+from .models import Category, Product
+from .serializer import (
+    CategorySerializer,
+    ProductSerializer,
+    ProductCreateSerializer,
+    CategoryCreateSerializer
+)
+from .services import get_serializable_queryset
 
 
-class ProductListView(ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductApiView(APIView):
+    def get(self, request):
+        category_slug = request.GET.get('category')
+        product_slug = request.GET.get('product', None)
+        data = get_serializable_queryset(category_slug=category_slug, product_slug=product_slug)
+        return Response(status=http.HTTPStatus.OK, data=data)
+
+
+class CategoryView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    lookup_field = 'slug'
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
 
 
 class ProductView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     lookup_field = 'slug'
     serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+
+
+class CategoryCreateView(CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryCreateSerializer
+    permission_classes = [IsAdminUser]
+
+
+class ProductCreateView(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductCreateSerializer
     permission_classes = [IsAdminUser]
